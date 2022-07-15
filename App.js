437376d20c56dadc0f864487a805fa2e96d620b7
willie <willie@gmail.com>
2022-07-15 14:17:17 +0800

    增加分頁連結

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Drawer } from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+
 import homeRoute from './src/home';
 import pageRoute from './src/page';
+import {  } from 'react-native-paper';
 
 export default class App extends Component{
   render(){
     return (
-      <View style={styles.container}>
-        
-      </View>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name='home' component={homeRoute}/>
+          <Drawer.Screen name='page' component={pageRoute}/>
+        </Drawer.Navigator>
+      </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
