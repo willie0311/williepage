f4674af52721bd8b610eef0cf29ef6d9313f37bb
willie <willie@gmail.com>
2022-07-15 14:13:43 +0800

    分頁一開始

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import homeRoute from './src/home';
+import pageRoute from './src/page';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
