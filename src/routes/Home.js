a2380064bf8af883be3120140b8441a749a7cdd2
willie <willie@gmail.com>
2022-07-18 10:20:06 +0800

    降板無法開啟的問題 處理完畢

---
+++ src/routes/Home.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+
+export default class Home extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
